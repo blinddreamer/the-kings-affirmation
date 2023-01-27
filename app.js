833f682e30547768d643dad0b490c838c4f81c8a
Nikolay Apostolov <blinddreamer.huku.rocks>
2023-01-27 16:58:43 +0200

    activityOptions - since how many times you can watch trigun ?!

--- app.js
+++ app.js
@@ -22,12 +22,25 @@ client.on('warn', (warning) => {
     console.warn(warning);
 });
 
+//client.on('ready', () => {
+//    const channel = client.channels.cache.get('490478387991805973');
+//    channel.send('back online');
+//});
+
+const activityOptions = [
+    { name: 'Trigun Stampede', type: ActivityType.Watching },
+    { name: 'BattleBots', type: ActivityType.Watching },
+    { name: 'Cowboy Bebop', type: ActivityType.Listening },
+];
+
 client.on('ready', () => {
     console.log('setting status');
+    const randomIndex = Math.floor(Math.random() * activityOptions.length);
+    const activity = activityOptions[randomIndex];
     client.user.setPresence({
-        activities: [{ name: `Trigun Stampede`, type: ActivityType.Watching }],
+        activities: [activity],
         status: 'online',
-      })
+    });
 });
 
 const config = [
@@ -57,10 +70,5 @@ const destroy = () => {
   process.on("SIGINT", destroy);
   process.on("SIGTERM", destroy);
   
-
-client.on('ready', () => {
-    const channel = client.channels.cache.get('490478387991805973');
-    channel.send('back online');
-});
 
 client.login(process.env.DISCORD_TOKEN);
