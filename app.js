7824cde48b58b70d710728fde77a8c8b7bcba2fb
Nikolay Apostolov <blinddreamer.huku.rocks>
2023-02-27 01:41:45 +0200

    MAJOR PATCH - MANY CHANGES

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const { ActivityType } = require('discord.js');
-const { ReactionRole } = require("discordjs-reaction-role");
+const { setupRoles } = require('./roles');
 
 const client = new Client({
-    partials: [Partials.Message, Partials.Reaction],
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMessageReactions,
-		GatewayIntentBits.MessageContent,
-	],
+  partials: [Partials.Message, Partials.Reaction],
+  intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.MessageContent,
+  ],
 });
 
 client.on('error', (error) => {
@@ -25,6 +25,8 @@ client.on('warn', (warning) => {
 //    channel.send('back online');
 //});
 
+
+//roles
 const activityOptions = [
     { name: 'Trigun Stampede', type: ActivityType.Watching },
     { name: 'BattleBots', type: ActivityType.Watching },
@@ -32,42 +34,15 @@ const activityOptions = [
 ];
 
 client.on('ready', () => {
-    console.log('setting status');
-    const randomIndex = Math.floor(Math.random() * activityOptions.length);
-    const activity = activityOptions[randomIndex];
-    client.user.setPresence({
-        activities: [activity],
-        status: 'online',
-    });
+  const randomIndex = Math.floor(Math.random() * activityOptions.length);
+  const activity = activityOptions[randomIndex];
+  client.user.setPresence({
+    activities: [activity],
+    status: 'online',
+  });
+
+  // Call the setupRoles function and pass the client object as a parameter
+  setupRoles(client);
 });
-
-const config = [
-    {
-      messageId: "818172637393190933",  //plex
-      roleId: "759091494215745557",
-      reaction: "759103156285472818",
-    },
-    {
-      messageId: "820675271949680710",  //sot
-      roleId: "627688811966758922",
-      reaction: "759103156218757162",
-    },
-    {
-      messageId: "820677513205383168", //sc
-      roleId: "759093067772067890",
-      reaction: "759103161276432384",
-    },
-  ];
-
-const manager = new ReactionRole(client, config); 
-
-const destroy = () => {
-    manager.teardown();
-    client.destroy();
-  };
-
-process.on("SIGINT", destroy);
-process.on("SIGTERM", destroy);
-  
 
 client.login(process.env.DISCORD_TOKEN);
---
+++ roles.js
@@ -0,0 +1,35 @@
+// roles.js
+
+const { ReactionRole } = require("discordjs-reaction-role");
+
+function setupRoles(client) {
+  const config = [
+    {
+      messageId: "818172637393190933",  //plex
+      roleId: "759091494215745557",
+      reaction: "759103156285472818",
+    },
+    {
+      messageId: "820675271949680710",  //sot
+      roleId: "627688811966758922",
+      reaction: "759103156218757162",
+    },
+    {
+      messageId: "820677513205383168", //sc
+      roleId: "759093067772067890",
+      reaction: "759103161276432384",
+    },
+  ];
+
+  const manager = new ReactionRole(client, config); 
+
+  const destroy = () => {
+    manager.teardown();
+    client.destroy();
+  };
+
+  process.on("SIGINT", destroy);
+  process.on("SIGTERM", destroy);
+}
+
+module.exports = { setupRoles };
