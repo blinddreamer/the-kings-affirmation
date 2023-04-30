fcf80c8a8d8dc94ade4f7c3165f874c41b05adbc
BlindDreamer <blinddreamer@huku.rocks>
2023-04-30 07:11:47 +0300

    more stuffo

--- activityOptions.js
+++ activityOptions.js
@@ -4,6 +4,9 @@ const activityOptions = [
   { name: 'Trigun Stampede', type: ActivityType.Watching },
   { name: 'BattleBots', type: ActivityType.Watching },
   { name: 'Cowboy Bebop', type: ActivityType.Listening },
+  { name: 'Conculluela, O`Neill - Legendario (feat. O`Neill)', type: ActivityType.Listening },
+  { name: 'Music Factory - BLOOD STREETS TEARS(SPEEDY CORE) [Remix]', type: ActivityType.Listening },
+  { name: 'Dimension - if You Want To - Remix', type: ActivityType.Listening },
 ];
 
 module.exports = activityOptions;
