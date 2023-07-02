89827466d91f87e9695e229a956c50dac075dcad
BlindDreamer <blinddreamer@huku.rocks>
2023-07-02 06:10:48 +0300

    bbb

--- activityOptions.js
+++ activityOptions.js
@@ -4,15 +4,15 @@ const { ActivityType } = require('discord.js');
 
 const activityOptions = [
   { name: 'SaltEMike Reacts', type: ActivityType.Watching },
-  { name: 'reddit', type: ActivityType.Watching },
+  { name: 'MANDO!', type: ActivityType.Watching },
   { name: 'Once in a Six Side', type: ActivityType.Watching },
   { name: 'NIGHTRIDE', type: ActivityType.Watching },
   { name: 'Trigun Stampede', type: ActivityType.Watching },
   { name: 'BattleBots', type: ActivityType.Watching },
   { name: 'GTA5rp', type: ActivityType.Watching },
-  { name: 'Cowboy Bebop', type: ActivityType.Listening },
-  { name: 'Cosculluela, O`Neill - Legendario (feat. O`Neill)', type: ActivityType.Listening },
-  { name: 'Music Factory - BLOOD STREETS TEARS(SPEEDY CORE) [Remix]', type: ActivityType.Listening },
+  { name: 'Cowboy Bebop OS', type: ActivityType.Listening },
+  { name: 'Cosculluela, O`Neill - Legendario', type: ActivityType.Listening },
+  { name: 'YASS - En La Isla ', type: ActivityType.Listening },
   { name: 'Dimension - if You Want To - Remix', type: ActivityType.Listening },
 ];
 
