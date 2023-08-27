3fa921998e83d3ca5cd007603b46ce22513ead5b
Nikolay Apostolov <blinddreamer.huku.rocks>
2023-08-27 18:20:58 +0300

    change lib ai

--- components/smart.js
+++ components/smart.js
@@ -1,20 +1,19 @@
-// smart.js
-const OpenAI = require("openai-api");
+const tectalicOpenai = require("@tectalic/openai").default;
 
-const OPENAI_API_KEY = process.env.SMART_KEY;
+// Load your OpenAI API key from an environment variable
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY; // Replace with your environment variable
 
-const openai = new OpenAI({ key: OPENAI_API_KEY });
+// Create an OpenAI client
+const openai = tectalicOpenai(OPENAI_API_KEY);
 
 async function answerQuestion(question) {
   try {
-    const gptResponse = await openai.complete({
-      engine: "davinci",
-      prompt: question,
-      maxTokens: 250,
-      // Additional parameters can be added here as needed
+    const gptResponse = await openai.chatCompletions.create({
+      model: "gpt-4", // Choose the model you want to use, e.g., 'gpt-4'
+      messages: [{ role: "user", content: question }],
     });
 
-    return gptResponse.choices[0].text;
+    return gptResponse.data.choices[0].message.content.trim();
   } catch (error) {
     console.error("Error fetching response from OpenAI:", error.message);
     throw error;
