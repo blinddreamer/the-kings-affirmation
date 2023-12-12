ce828c2cd5aa1877301ae5fcde255d3c90d89d45
BlindDreamer <blinddreamer@huku.rocks>
2023-12-12 03:07:08 +0200

    Structural Salvage VFX Polish pass

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { handleMessage } = require("./components/smart");
 const { getRandomActivity } = require("./components/activityOptions");
 const { checkYouTubeChannels } = require("./components/tube");
 
+// Create a new instance of the Discord client
 const client = new Client({
   partials: [Partials.Message, Partials.Reaction],
   intents: [
@@ -16,6 +17,7 @@ const client = new Client({
   ],
 });
 
+// Event handling for errors and warnings
 client.on("error", (error) => {
   console.error("Bot error:", error);
 });
@@ -24,9 +26,12 @@ client.on("warn", (warning) => {
   console.warn("Bot warning:", warning);
 });
 
+// Function to set up roles
 setupRoles(client);
 
+// Event handling when the bot is ready
 client.on("ready", () => {
+  // Set a random activity when the bot starts
   const activity = getRandomActivity();
   client.user.setPresence({
     activities: [activity],
@@ -34,26 +39,35 @@ client.on("ready", () => {
   });
 
   console.log(`Bot is ready as ${client.user.tag}`);
-  console.log(`Setting activity`);
+  console.log(`Setting initial activity`);
 
-  setupRoles(client);
-
+  // Update activity every 6 hours
   setInterval(() => {
-    const newActivity = getRandomActivity(); // Update activity periodically
+    const newActivity = getRandomActivity();
     client.user.setActivity(newActivity);
+    console.log(`Updated activity to: ${newActivity.name}`);
   }, 6 * 60 * 60 * 1000);
 
   // Specify the target channel ID
   const targetChannelId = "482315570201755664";
 
-  // Call checkYouTubeChannels every 168h(1week) for the specified channel
-  setInterval(() => {
+  // Schedule the checkYouTubeChannels function to run every Friday at 9 PM
+  const fridayNinePM = new Date();
+  fridayNinePM.setHours(21, 0, 0, 0);
+  const timeUntilFridayNine = fridayNinePM.getTime() - Date.now();
+  setTimeout(() => {
     checkYouTubeChannels(client, targetChannelId);
-  }, 168 * 60 * 60 * 1000);
+    // Schedule the next run for the following Friday at 9 PM
+    setInterval(() => {
+      checkYouTubeChannels(client, targetChannelId);
+    }, 7 * 24 * 60 * 60 * 1000);
+  }, timeUntilFridayNine);
 });
 
+// Event handling for incoming messages
 client.on("messageCreate", (message) => {
-  handleMessage(message, client); // Pass the client object to handleMessage
+  handleMessage(message, client);
 });
 
+// Log in to Discord using the provided token
 client.login(process.env.DISCORD_TOKEN);
